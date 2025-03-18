a79bf7ab0d2e20d5fc6ec8c8a26dfc0939352384
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2025-03-18 17:41:17 +0200

    Added log severity tags

--- include/Utility/Logging.h
+++ include/Utility/Logging.h
@@ -7,7 +7,20 @@ namespace PS {
     template <RC::Unreal::int32 optional_arg, typename... FmtArgs>
     auto Log(RC::File::StringViewType content, FmtArgs... fmt_args) -> void
     {
-        auto formatted_log = std::format(STR("[PalSchema] {}"), content);
-        RC::Output::send<optional_arg>(formatted_log, fmt_args...);
+        if (optional_arg == RC::LogLevel::Error)
+        {
+            auto formatted_log = std::format(STR("[PalSchema] [error] {}"), content);
+            RC::Output::send<optional_arg>(formatted_log, fmt_args...);
+        }
+        else if (optional_arg == RC::LogLevel::Warning)
+        {
+            auto formatted_log = std::format(STR("[PalSchema] [warning] {}"), content);
+            RC::Output::send<optional_arg>(formatted_log, fmt_args...);
+        }
+        else
+        {
+            auto formatted_log = std::format(STR("[PalSchema] {}"), content);
+            RC::Output::send<optional_arg>(formatted_log, fmt_args...);
+        }
     }
 }
