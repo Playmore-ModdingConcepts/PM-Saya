8880ee1727a85ba24d656efd6413f6bf9d954f0a
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2025-03-29 03:40:23 +0200

    Add a check for DebugConsoleVisible before doing ImGui logic

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -27,21 +27,6 @@ public:
         Palworld::SignatureManager::Initialize();
         MainLoader.PreInitialize();
 
-        register_tab(STR("Pal Schema"), [](CppUserModBase* instance) {
-            auto mod = dynamic_cast<PalSchema*>(instance);
-            if (!mod)
-            {
-                return;
-            }
-
-            if (ImGui::Button("Reload Schema Mods"))
-            {
-                UECustom::AsyncTask(UECustom::ENamedThreads::GameThread, [mod]() {
-                    mod->reload_mods();
-                });
-            }
-        });
-
         PS::Log<RC::LogLevel::Verbose>(STR("{} v{} by {} loaded.\n"), ModName, ModVersion, ModAuthors);
     }
 
@@ -56,7 +41,25 @@ public:
 
     auto on_ui_init() -> void override
     {
-        UE4SS_ENABLE_IMGUI()
+        if (UE4SSProgram::settings_manager.Debug.DebugConsoleVisible)
+        {
+            UE4SS_ENABLE_IMGUI()
+
+            register_tab(STR("Pal Schema"), [](CppUserModBase* instance) {
+                auto mod = dynamic_cast<PalSchema*>(instance);
+                if (!mod)
+                {
+                    return;
+                }
+
+                if (ImGui::Button("Reload Schema Mods"))
+                {
+                    UECustom::AsyncTask(UECustom::ENamedThreads::GameThread, [mod]() {
+                        mod->reload_mods();
+                    });
+                }
+            });
+        }
     }
 
     auto on_update() -> void override
