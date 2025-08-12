69ce21af6ca9724ff3e131ea8b7e1bcafc30b24e
Okaetsu <mail@okaetsu.dev>
2025-08-12 10:07:46 +0300

    Update docusaurus.config.ts

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -73,13 +73,6 @@ const config: Config = {
           label: 'Documentation',
         },
         {
-          href: 'https://discord.gg/SZFBGuWMEU',
-          position: 'right',
-          className: 'header-discord-link',
-          'aria-label': 'Discord Server',
-          'title': 'Join the Discord Server'
-        },
-        {
           href: 'https://github.com/Okaetsu/PalSchema',
           position: 'right',
           className: 'header-github-link',
